987ab94facea18f62b69ed18288ab2afa0a71298
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-04 23:30:14 -0400

    added customers table

---
+++ database/migrations/2024_07_05_025211_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->integer('pickup_day'); // 0 - Sunday, 1 - Monday, 6 - Saturday
+            $table->integer('pickup_frequency')->default(1); // in weeks. 1 is every week, 2 ever other, etc.
+            $table->string('contact_method'); // E.g., "email", "text"
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
