22a8907b97318056aec463185594a34dab38128f
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-05 13:31:23 -0400

    catch all route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+//catch all route
+Route::get('/{vue_capture?}', function () {
+    return view('welcome');
+})->where('vue_capture', '[\/\w\.-]*');
+
 Route::get('/', function () {
     return view('welcome');
 });
