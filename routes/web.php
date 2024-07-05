b44168ba6a1e1b125531fc98bd13dbfe8389d7b2
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-05 16:51:03 -0400

    routes for driver and admin views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +24,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    // Admin routes
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::post('/api/admin/customers/{customer}/reminder', [AdminController::class, 'sendReminder']);
+
+    // Driver routes
+    Route::get('/driver', [DriverController::class, 'dashboard'])->name('driver.dashboard');
+    Route::post('/api/driver/customers/{customer}/claim', [DriverController::class, 'claimPickup']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
