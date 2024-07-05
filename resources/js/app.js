d98b57164e5258683f987679b4a27b5310facce5
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-05 13:18:18 -0400

    vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+// import router from './router';
+import router from './router/index.js';
+
+createApp(App).use(router).mount('#app');
