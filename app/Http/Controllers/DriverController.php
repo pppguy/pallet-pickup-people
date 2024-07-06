bf2fdf42f32fe9a6d79f3ab2a62a8127697f02e9
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-06 02:41:59 -0400

    working on getting the proper pickup dates

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -4,24 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Customer;
+use App\Models\CustomerPrompt;
+use App\Models\DriverPickup;
+use Carbon\Carbon;
 use Inertia\Inertia;
 
 class DriverController extends Controller
 {
     public function dashboard()
     {
-        $customers = Customer::where('status', 'pending')->get();
+        // $customers = CustomerPrompt::all();
+        // return Inertia::render('DriverDashboard', [
+        //     'customers' => $customers,
+        // ]);
+
+        //$this->getCustomersYES();
+
+        //$pickups = DriverPickup::all();
+
+
+    }
+
+    public function claimPickup($pickupId)
+    {
+        // Logic to claim the pickup
+        $pickup = DriverPickup::find($pickupId);
+        $pickup->status = 'claimed'; // Update status as needed
+        $pickup->save();
+        return response()->json(['message' => 'Pickup claimed!']);
+    }
+
+    public function confirmPickup($pickupId)
+    {
+    }
+
+    public function getCustomersYES()
+    {
+        $customers = CustomerPrompt::where('response', 'YES')->get();
         return Inertia::render('DriverDashboard', [
             'customers' => $customers,
         ]);
     }
 
-    public function claimPickup($customerId)
+    public function getUpcomingPickups()
     {
-        // Logic to claim the pickup
-        $customer = Customer::find($customerId);
-        $customer->status = 'claimed'; // Update status as needed
-        $customer->save();
-        return response()->json(['message' => 'Pickup claimed!']);
+        // Get today's date
+        $today = Carbon::today();
+
+        //only get pickups where the pickup date is today or later
+        $pickups = DriverPickup::where('pickup_date', '>=', $today)->get();
+
+        return $pickups;
     }
 }
