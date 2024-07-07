56fef1c117d7c2ee558019d2afccc31b23ec4ed9
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-06 20:42:28 -0400

    claim button amber

--- resources/js/Pages/DriverDashboard.vue
+++ resources/js/Pages/DriverDashboard.vue
@@ -42,7 +42,7 @@
                   <td class="px-6 py-4 whitespace-nowrap">
                     <button 
                       v-if="!pickup.driver"
-                      class="bg-green-500 text-white px-4 py-2 rounded w-28" 
+                      class="bg-amber-300 text-white px-4 py-2 rounded w-28" 
                       @click="claimPickup(pickup.id)"
                       
                     >                    
