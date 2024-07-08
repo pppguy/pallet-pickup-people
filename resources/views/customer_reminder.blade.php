384f608d26e7711e4d72cf42a5306ade35838fe9
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-08 01:10:13 -0400

    email template

---
+++ resources/views/customer_reminder.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Customer Reminder</title>
+</head>
+<body>
+    <p>Dear Customer,</p>
+    <p>Please respond to this email by clicking one of the buttons below:</p>
+    <a href="{{ $yesUrl }}">YES</a>
+    <a href="{{ $noUrl }}">NO</a>
+</body>
+</html>
