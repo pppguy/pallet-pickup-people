d9b9fcffc4c0c00a93bae3f2ae9247e39500254e
Hur Romanchik <hromanchik1213@gmail.com>
2024-07-08 11:38:28 -0400

    full width text boxes

--- resources/js/Pages/AdminDashboard.vue
+++ resources/js/Pages/AdminDashboard.vue
@@ -140,7 +140,7 @@ const getCurrentStatus = (latestPrompt) => {
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-start">
+            <div class="">
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">Edit Customer</h3>
                 <div class="mt-2">
